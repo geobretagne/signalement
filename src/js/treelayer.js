848ee34756ab37c75157366a1c4cf6eb6fd3d470
spelhate <seb.pelhate@gmail.com>
2014-05-19 17:04:51 +0200

    Add license

--- src/js/treelayer.js
+++ src/js/treelayer.js
@@ -1,4 +1,16 @@
-﻿Ext.namespace("Signalement");
+/*
+ * 
+ * This file is part of signalement
+ *
+ * signalement is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with signalement.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Signalement");
 Signalement.treelayer = (function () {
 
     /*
