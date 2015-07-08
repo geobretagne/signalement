cba5050fd1c45d622d6eec0ab3c1214084c90af8
bmalan <bj.malan@yahoo.fr>
2015-07-08 11:29:42 +0200

    ajout de fonctionnalités/réglages
    1) ajout d'un EPSG (3948)
    2) Configuration du WFS signalement
    3) ajout du paramètre ?feature= dans les URL (ligne 
    4) Rendre le panel de fonctionnalité "collapsible" (=rétractable).

--- src/js/main.js
+++ src/js/main.js
@@ -48,9 +48,10 @@ Signalement.main = (function () {
         Proj4js.defs["EPSG:3857"] = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs";        
         Proj4js.defs["EPSG:2154"] = "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
         Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-		
+	Proj4js.defs["EPSG:3948"] = "+proj=lcc +lat_1=47.25 +lat_2=48.75 +lat_0=48 +lon_0=3 +x_0=1700000 +y_0=7200000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
+       	
 		var today = new Date();        
-        var beginDate = new Date(new Date().add(Date.MONTH, -6));        
+        var beginDate = new Date(new Date().add(Date.MONTH, -12));        
         var wfsfilter = new OpenLayers.Filter.Comparison({
                 type: OpenLayers.Filter.Comparison.BETWEEN,
                 property: "date_saisie",
@@ -70,14 +71,14 @@ Signalement.main = (function () {
             strategies: strategies,
             filter: wfsfilter,
             protocol: new OpenLayers.Protocol.WFS({
-                url: config.workinglayer.wfsurl,
+                "https://www.cigalsace.org/geoserver/cigal_edit/wfs?",
                 version: "1.1.0",
-                featureType: config.workinglayer.featuretype,
-                featurePrefix: config.workinglayer.featureprefix,
+                featureType: "signalement_adresse",
+                featurePrefix: "cigal_edit",
                 srsName: "EPSG:3857",
-                geometryName: config.workinglayer.geometryname,
+                geometryName: "the_geom",
                 extractAttributes: true,
-                schema: config.workinglayer.wfsurl + "service=WFS&version=1.1.0&request=DescribeFeatureType&TypeName=" + config.workinglayer.featuretype
+                schema: "https://www.cigalsace.org/geoserver/cigal_edit/wfs?service=WFS&version=1.1.0&request=DescribeFeatureType&TypeName=signalement_adresse"
             })
         });		
 		
@@ -111,24 +112,43 @@ Signalement.main = (function () {
         toolbar.add([Signalement.toolbar.create(map, config)]);
 
         var mapPanelOptions = {
-            title: "SIGN'ADRESSE - Gestion des signalements",
+            title: "CIGAL - SIGN'ADRESSE - Gestion des signalements",
             region: "center",
             id: "myMapPanel",
             height: 400,
-            width: 600,
+            width: 550,
             map: map,
             layers: map.layers,
             items: [zSlider],
             tbar: toolbar
         }
 
-        // définition dynamique du baseLayer et de l'extent
+        // définition dynamique du baseLayer et de l'extent, et du zoom sur featureID
+        //feature
+		if (urlParameters['feature']) {
+			var fi = urlParameters['feature'];
+			
+			//requete pour récupérer la géométrie du signalement fi
+		var requetehttppost = new XMLHttpRequest();
+		var url = "https://www.cigalsace.org/geoserver/cigal_edit/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cigal_edit:signalement_adresse&outputFormat=application/json&srsName=EPSG:3857&FeatureId=" + fi;
+		requetehttppost.open("GET", url, false);
+		requetehttppost.setRequestHeader("Content-type", " application/json; charset=UTF-8");
+		requetehttppost.send(null);
+			
+			var feat = JSON.parse(requetehttppost.responseText);
+			mapPanelOptions.zoom = 17;
+            mapPanelOptions.center = new OpenLayers.LonLat(feat.features[0].geometry.coordinates[0],feat.features[0].geometry.coordinates[1]);
+		
+			var bs = map.getLayersByName('Photo aérienne (CUS CAC M2A CIGAL)');
+            map.setBaseLayer(bs[0]);
+        }
+        //baselayer
         if (urlParameters['baselayer']) {
             var bs = map.getLayersByName(urlParameters['baselayer']);
             if (bs.length > 0) {
                 map.setBaseLayer(bs[0]);
             } else {
-                alert("le baseLayer " + urlParameters['baslayer'] + "n'est pas valide");
+                alert("le baseLayer " + urlParameters['baselayer'] + "n'est pas valide");
             }
         }
         //Extent
@@ -140,9 +160,9 @@ Signalement.main = (function () {
             mapPanelOptions.extent = new OpenLayers.Bounds(bbox[0], bbox[1], bbox[2], bbox[3]);            
         }
         
-        else {
-            mapPanelOptions.center = new OpenLayers.LonLat(-333445.31476491, 6118631.2393202);           
-            mapPanelOptions.zoom = 8;
+        else if (!urlParameters['feature']) {
+            mapPanelOptions.center = new OpenLayers.LonLat(826933.99050,6151040.54017);           
+            mapPanelOptions.zoom = 8.5;
         }
         
         
@@ -150,7 +170,7 @@ Signalement.main = (function () {
         var mapPanel = new GeoExt.MapPanel(mapPanelOptions);
 
         // initialisations des différents modules
-        var signalFormWindow = Signalement.signalement.create(map, mapPanel, toolbar, WFSSignal, true, phplocation,config.htmltexts[7],cluster);
+        var signalFormWindow = Signalement.signalement.create(map, mapPanel, toolbar, WFSSignal, true, phplocation,config.htmltexts[10],cluster);
         Signalement.geocoder.create(map, toolbar, config.geocode.service);
         var rssPanel = Signalement.rss.create(map, config.workinglayer.rssurl, config.htmltexts, toolbar);
         var downloadForm = Signalement.download.create(config.workinglayer);
@@ -159,7 +179,7 @@ Signalement.main = (function () {
         var layerPanel = Signalement.treelayer.create(map, config.workinglayer.label);
         var csvUploadForm = Signalement.importer.create(map, WFSSignal, phplocation);
         var workflowForm = Signalement.workflow.create(map, WFSSignal, phplocation);        
-		var filterPanel = Signalement.remotefilter.create(map, WFSSignal, toolbar, -6);
+		var filterPanel = Signalement.remotefilter.create(map, WFSSignal, toolbar, -12); //taille de la barre de date
 
         //    Création du panel avancé
         var advancedPanel = new Ext.Panel({
@@ -170,9 +190,9 @@ Signalement.main = (function () {
             minWidth: 175,
             maxWidth: 400,
             layout: 'accordion',
-            collapsible: false,
+            collapsible: true,
             collapsed: false,
-            animCollapse: false,
+            animCollapse: true,
             split: false
         });
 
